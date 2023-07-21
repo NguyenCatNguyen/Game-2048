d08711f6e68f5f86969af420e594e3d66a2ceba3
NguyenCatNguyen <catngn11@gmail.com>
2023-07-21 18:18:40 -0500

    update

--- Grid.js
+++ Grid.js
@@ -1,13 +1,17 @@
 
-const grid_size = 4;
-const cell_size = 20;
-const cell_gap = 2;
+const GRID_SIZE = 4
+const CELL_SIZE = 20
+const CELL_GAP = 2
 
 
 export default class Grid {
     constructor(gridElement){
-        gridElement.setProperty("--grid-size", grid_size);
-        gridElement.setProperty("--cell-size", `${cell_size}vmin`);
-        gridElement.setProperty("--cell-gap", `${cell_gap}vmin`)
+        gridElement.setProperty("--grid-size",GRID_SIZE);
+        gridElement.setProperty("--cell-size",`${CELL_SIZE}vmin`);
+        gridElement.setProperty("--cell-gap",`${CELL_GAP}vmin`);
+        createCellElements(gridElement);
     }
+}
+function createCellElements(gridElement){
+    
 }
--- scripts.js
+++
@@ -1,4 +0,0 @@
-import Grid from './Grid.js';
-const Board = document.getElementById('board');
-
-const grid = new Grid(Board);
